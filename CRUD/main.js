e037ba6d2a303ec1f8a8397f96cd228522cdc383
Chi Xu <stevenxu9494@gmail.com>
2020-06-26 15:31:21 +1000

    CRUD Completed

--- CRUD/main.js
+++ CRUD/main.js
@@ -1,10 +1,90 @@
 var app = new Vue({
   el: '#app',
   data: {
-    errorMsg: false,
-    successMsg: false,
+    errorMsg: "",
+    successMsg: "",
     showAddModal: false,
     showEditModal: false,
-    showDeleteModal:false
+    showDeleteModal:false,
+    users:[],
+    newUser:{name:"", address:"", mobile:""},
+    currentUser:{}
+  },
+
+  mounted: function() {
+    this.getAllUsers();
+  },
+
+  methods:{
+    getAllUsers() {
+      axios.get("http://localhost:8000/CRUD/process.php?action=read").then(function(response){
+        if(response.data.error){
+          app.errorMsg = response.data.message;
+        }
+        else{
+          app.users=response.data.users;
+        }
+      });
+    },
+    // add new user
+    addUser(){
+      var formData = app.toFormData(app.newUser);
+      axios.post("http://localhost:8000/CRUD/process.php?action=create", formData).then(function(response){
+        app.newUser = {name: "", address: "", mobile:""};
+        if(response.data.error){
+          app.errorMsg = response.data.message;
+        }
+        else{
+          app.successMsg = response.data.message;
+          app.getAllUsers();
+        }
+      });
+    },
+
+    // update user
+    updateUser(){
+      var formData = app.toFormData(app.currentUser);
+      axios.post("http://localhost:8000/CRUD/process.php?action=update", formData).then(function(response){
+        app.currentUser = {};
+        if(response.data.error){
+          app.errorMsg = response.data.message;
+        }
+        else{
+          app.successMsg = response.data.message;
+          app.getAllUsers();
+        }
+      });
+    },
+
+    // update user
+    deleteUser(){
+      var formData = app.toFormData(app.currentUser);
+      axios.post("http://localhost:8000/CRUD/process.php?action=delete", formData).then(function(response){
+        app.currentUser = {};
+        if(response.data.error){
+          app.errorMsg = response.data.message;
+        }
+        else{
+          app.successMsg = response.data.message;
+          app.getAllUsers();
+        }
+      });
+    },
+    // a helper in order to get data and append to this object
+    toFormData(obj){
+      var fd = new FormData();
+      for(var i in obj){
+        fd.append(i, obj[i]);
+      }
+      return fd;
+    },
+    // a helper to select current user
+    selectUser(user){
+      app.currentUser = user;
+    },
+    clearMsg(){
+      app.errorMsg = "";
+      app.successMsg = "";
+    }
   }
 })
---
+++ CRUD/process.php
@@ -0,0 +1,76 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "aumake";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $database);
+  
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  // echo "Connected successfully";
+
+  // get result
+  $result = array('error' => false);
+  $action = '';
+
+  $action = isset($_GET['action']) ? $_GET['action'] :NULL;
+
+  if($action == 'read'){
+    $sql = $conn->query("SELECT * FROM users");
+    $users = array();
+    while($row=$sql->fetch_assoc()){
+      array_push($users, $row);
+    }
+    $result['users'] = $users;
+  }
+
+  if($action == 'create'){
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $mobile = $_POST['mobile'];
+
+    $sql = $conn->query("INSERT INTO users (name,address,mobile) VALUES('$name', '$address', '$mobile')");
+
+    if($sql) {
+      $result['message'] = "User added successfully!";
+    }
+    else {
+      $result['error'] = true;
+      $result['message'] = "Failed to add user!";
+    }
+  }
+
+  if($action == 'update'){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $mobile = $_POST['mobile'];
+    $sql = $conn->query("UPDATE users SET name='$name', address='$address', mobile='$mobile' WHERE id='$id'");
+    if($sql) {
+      $result['message'] = "User updated successfully!";
+    }
+    else {
+      $result['error'] = true;
+      $result['message'] = "Failed to update user!";
+    }
+  }
+
+  if($action == 'delete'){
+    $id = $_POST['id'];
+    $sql = $conn->query("DELETE FROM users WHERE id='$id'");
+    if($sql) {
+      $result['message'] = "User deleted successfully!";
+    }
+    else {
+      $result['error'] = true;
+      $result['message'] = "Failed to delete user!";
+    }
+  }
+
+  $conn->close();
+  echo json_encode($result);
+?>
