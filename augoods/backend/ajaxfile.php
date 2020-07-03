3b7c7b78e10f7a1cfcd743e55412e62286c93a58
Chi Xu <stevenxu9494@gmail.com>
2020-07-03 11:18:28 +1000

    php array -> json

--- augoods/backend/ajaxfile.php
+++ augoods/backend/ajaxfile.php
@@ -2,26 +2,23 @@
 
   include "config.php";
 
-  // fetch all products
+  
+  // query
   $sql = "SELECT * FROM products";
   $result = $conn->query($sql);
-  $products = array();
-  if ($result->num_rows > 0) {
-    // echo "<table><tr><th>id</th><th>name</th><th>price</th><th>sellPrice</th><th>brand</th><th>category</th><th>sales</th><th>netWeight</th><th>unit</th><th>thumbUrl</th><th>imageUrl</th></tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      // array_push($products,$row);
-      $products[] = $row;
-      echo json_encode($row);
-      // echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["price"]."</td><td>".$row["sellPrice"]."</td><td>".$row["brand"]."</td><td>".$row["category"]."</td><td>".$row["sales"]."</td><td>".$row["netWeight"]."</td><td>".$row["unit"]."</td><td>".$row["thumbUrl"]."</td><td>".$row["imageUrl"]."</td></tr>";
-    }
-    // echo "</table>";
-  } else {
-    echo "0 results";
-  }
 
-  echo json_encode($products,'UTF'-8);
+  $arr = array();
+  // 输出每行数据
+  while($row = $result->fetch_assoc()) {
+    $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小
+    for($i=0;$i<$count;$i++){
+        unset($row[$i]);//删除冗余数据
+    }
+    array_push($arr,$row);
+
+  }
+  //print_r($arr);
+  echo json_encode($arr,JSON_UNESCAPED_UNICODE);//json编码
   $conn->close();
-  // fetch products by category
 
 ?>
