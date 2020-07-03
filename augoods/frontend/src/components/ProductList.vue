c71b50e6d3f53d71e0c9229204bae40dcc428c12
Chi Xu <stevenxu9494@gmail.com>
2020-07-03 17:12:24 +1000

    pagination

--- augoods/frontend/src/components/ProductList.vue
+++ augoods/frontend/src/components/ProductList.vue
@@ -6,22 +6,31 @@
         <h4>
           {{p.name}}
           <span class="badge badge-pill badge-primary float-right">
-            {{ p.price }}
+            {{ p.price | currency}}
           </span>
         </h4>
         <div class="card-text bg-white p-1">{{ p.description }}</div>
       </div>
     </div>
+    <page-controls />
   </div>
 </template>
 
 <script>
 
-import { mapState } from "vuex";
+import { mapGetters } from "vuex";
+import PageControls from "./PageControls";
 
 export default {
+  components: {PageControls},
   computed: {
-    ...mapState(["products"])
+    ...mapGetters({ products: "processedProducts" })
+  },
+  filters: {
+    currency(value) {
+      return new Intl.NumberFormat("zh-CN",
+      { style: "currency", currency: "CNY"}).format(value);
+    }
   }
 }
 </script>
--- augoods/frontend/src/components/Store.vue
+++ augoods/frontend/src/components/Store.vue
@@ -2,12 +2,12 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col bg-dark text-white">
-        <a class="navbar-brand">SPORTS STORE</a>
+        <a class="navbar-brand">澳驰</a>
       </div>
     </div>
     <div class="row">
       <div class="col-3 bg-info p-2">
-        <h4 class="text-white m-2">Categories</h4>
+        <h4 class="text-white m-2">品种</h4>
       </div>
       <div class="col-9 p-2">
         <product-list />
