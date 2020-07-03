7e9212ae036fe270dc315157b17d278475e06aff
Chi Xu <stevenxu9494@gmail.com>
2020-07-03 17:21:14 +1000

    initial category commit

--- augoods/frontend/src/store/index.js
+++ augoods/frontend/src/store/index.js
@@ -21,14 +21,23 @@ export default new Vuex.Store({
     products: testData,
     productsTotal: testData.length,
     currentPage:1,
-    pageSize: 4
+    pageSize: 4,
+    currentCategory: "All"
   },
   getters:{
-    processedProducts:state=> {
-      let index = (state.currentPage - 1)* state.pageSize;
-      return state.products.slice(index,index+state.pageSize);
+    productsFilteredByCategory: state => state.products
+    .filter(p=>state.currentCategory == "All"
+    || p.category == state.currentCategory),
+
+    processedProducts:(state, getters) => {
+      let index = (state.currentPage -1) * state.pageSize;
+      return getters.productsFilteredByCategory
+      .slice(index, index + state.pageSize);
     },
-    pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
+    pageCount: (state, getters) =>
+    Math.ceil(getters.productsFilteredByCategory.length / state.pageSize), 
+    categories: state => ["All", 
+      ...new Set(state.products.map(p=>p.category).sort())]
   },
   mutations: {
     setCurrentPage(state, page) {
@@ -37,6 +46,10 @@ export default new Vuex.Store({
     setPageSize(state, size) {
       state.pageSize = size;
       state.currentPage = 1;
+    },
+    setCurrentCategory(state,category) {
+      state.currentCategory=category;
+      state.currentPage =1;
     }
   }
 })
