5ea2bc8a6a9548be7218c679b72e8c9337f19931
Chi Xu <stevenxu9494@gmail.com>
2020-07-03 13:54:39 +1000

    initial frontend commit

--- augoods/backend/ajaxfile.php
+++ augoods/backend/ajaxfile.php
@@ -6,8 +6,9 @@
   // query
   $sql = "SELECT * FROM products";
   $result = $conn->query($sql);
-
+  $response = array();
   $arr = array();
+
   // 输出每行数据
   while($row = $result->fetch_assoc()) {
     $count=count($row);//不能在循环语句中，由于每次删除row数组长度都减小
@@ -15,10 +16,16 @@
         unset($row[$i]);//删除冗余数据
     }
     array_push($arr,$row);
-
   }
-  //print_r($arr);
-  echo json_encode($arr,JSON_UNESCAPED_UNICODE);//json编码
+  // print_r($arr);
+  // reponse添加products所有object
+  $response['products'] = $arr;
+  $jsonResponse = json_encode($response,JSON_UNESCAPED_UNICODE);//json编码
+  echo $jsonResponse;
   $conn->close();
+
+  $fp = fopen('response.json', 'w');
+  fwrite($fp, $jsonResponse);
+  fclose($fp);
 
 ?>
